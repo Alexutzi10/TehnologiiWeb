acfe9cfa5b2fa16ae1eb1b73e92adb5a24fb1810
Alexutzi10 <georgealexandrusurugiu@gmail.com>
2024-10-08 17:30:02 +0300

    Variabile

---
+++ letConst.js
@@ -0,0 +1,10 @@
+// const test = 3;
+// test = '3';
+
+// console.log(test);
+let obj = {};
+obj.name = 'Alex';
+
+console.log(obj.name);
+
+obj = {...obj, nume:'Misu'};
---
+++ var.js
@@ -0,0 +1,17 @@
+function test() {
+    if (true) {
+        var variable = 1;
+        let let2 = 2;
+        const constraint = 3;
+    }
+
+    console.log(variable);
+    console.log(let2);
+    console.log(constraint);
+}
+
+//test();
+
+
+console.log(variable2);
+const variable2 = 2;
