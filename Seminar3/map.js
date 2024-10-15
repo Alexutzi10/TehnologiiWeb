37a7e801ae357ff989672ec88b8918bc1e09c50d
Alexutzi10 <georgealexandrusurugiu@gmail.com>
2024-10-15 17:36:49 +0300

    Array manipulation

---
+++ Seminar3/map.js
@@ -0,0 +1,14 @@
+const array = [1, 2, 3, 4];
+
+const forEachArray = array.forEach(item => {
+    // console.log(item);
+    return item;
+})
+
+const mapped = array.map(item => {
+    // console.log(item);
+    return item;
+})
+
+console.log(forEachArray);
+console.log(mapped);
