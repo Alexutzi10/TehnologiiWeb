b6e51480ce966af1a609359362953062d8309509
Alexutzi10 <surugiugeorge22@stud.ase.ro>
2024-10-19 15:10:04 +0300

    -

--- arr.js
+++ arr.js
@@ -1,28 +1,27 @@
 const arr = [1, 2, 3, 4];
 
-//console.log(arr.length);
+console.log(arr.length);
 
-// arr.push(5);
+arr.push(5);
 
-// // arr = [1];
-// const lastItem = arr.p
-// op();
+// arr = [1];
+const lastItem = arr.pop();
 
-// arr.shift();
+arr.shift();
 
-// console.log(arr);
+console.log(arr);
 
-// for (let i=0; i<arr.length; i++) {
-//     console.log(arr[i]);
-// }
+for (let i=0; i<arr.length; i++) {
+    console.log(arr[i]);
+}
 
-// for (item of arr) {
-//     console.log(item);
-// }
+for (item of arr) {
+    console.log(item);
+}
 
-// for (item in arr) {
-//     console.log(item);
-// }
+for (item in arr) {
+    console.log(item);
+}
 
 arr.forEach(item => {
     console.log(item);
